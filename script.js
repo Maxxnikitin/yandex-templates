27347529cb6d17ae24e030938fa260ed9662ef95
Maxx Nikitin <maxxnikitin90@yandex.ru>
2022-09-27 23:37:52 +0700

    feat: add elements on page

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const items = [
   { text: "Пройти туториал по Реакту" },
   { text: "Сделать фронтенд для своего проекта" },
 ];
+
+const container = document.querySelector(".list");
+const template = document.querySelector(".template");
+
+const render = () => {
+  items.forEach((item) => {
+    const currentItem = createItemNode(item.text);
+    container.append(currentItem);
+  });
+};
+
+const createItemNode = (text) => {
+  const currentItem = template.content.cloneNode(true);
+  const currentText = currentItem.querySelector(".item__text");
+  currentText.textContent = text;
+
+  return currentItem;
+};
+
+render();
